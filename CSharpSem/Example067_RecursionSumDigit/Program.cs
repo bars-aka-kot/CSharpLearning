f486a75786e64e3a7ef85566342b5ad459034e4a
Basirov <basirov_ilgiz@mail.ru>
2022-11-18 19:57:51 +0500

    Add Example063, 065, 067, 069

---
+++ CSharpSem/Example067_RecursionSumDigit/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+// Console.Clear();
+int number = InputNumbers("Введите целое число: ");
+
+int SumDigits(int num)
+{
+    if (num == 0) return 0;
+    return num % 10 + SumDigits(num / 10);
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+int sumDigits = SumDigits(number);
+Console.Write($"Сумма цифр числа {number} равна {sumDigits}");
