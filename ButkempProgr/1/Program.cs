fd1291541e9627de14b9357b064a35eecf968c77
Basirov <basirov_ilgiz@mail.ru>
2023-01-11 00:51:17 +0500

    ButCamp

---
+++ ButkempProgr/1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+// Вычислить максимальную сумму 3-х элементов массива, стоящих подряд
+// array (произвольный, количество произвольно)
+// m = 3
+
+int size = 100000;
+
+int m = 300;
+
+int[] array = Enumerable.Range(1, size).Select(item => Random.Shared.Next(10)).ToArray();
+
+// Console.WriteLine($"[{string.Join(", ", array)}]");
+
+Stopwatch sw = new();
+sw.Start();
+
+int max = 0;
+for (int i = 0; i < array.Length - m; i++)
+{
+    int t = 0;
+    for (int j = i; j < i + m; j++)
+    {
+        t += array[j];
+    }
+    if (t > max)
+        max = t;
+}
+sw.Stop();
+Console.WriteLine($"time = {sw.ElapsedMilliseconds}");
+Console.WriteLine($"Способ 1 = {max}");
+
+int max1 = 0;
+for (int j = 0; j < m; j++)
+    max1 += array[j];
+int t1 = max1;
+for (int i = 1; i < array.Length - m; i++)
+{
+    t1 = t1 - array[i - 1] + array[i + (m - 1)];
+    if (t1 > max1)
+        max1 = t1;
+}
+
+Console.WriteLine(($"Способ 1 = {max1}"));
---
+++ ButkempProgr/2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+// Вычислить максимальную сумму 3-х элементов массива, стоящих подряд
+// array (произвольный, количество произвольно)
+// m = 3
+
+int size = 1000000;
+
+int m = 30000;
+
+int[] array = Enumerable.Range(1, size).Select(item => Random.Shared.Next(10)).ToArray();
+
+// Console.WriteLine($"[{string.Join(", ", array)}]");
+
+Stopwatch sw = new();
+sw.Start();
+
+int max = 0;
+for (int j = 0; j < m; j++)
+    max += array[j];
+int t = max;
+for (int i = 1; i < array.Length - m; i++)
+{
+    t = t - array[i - 1] + array[i + (m - 1)];
+    if (t > max)
+        max = t;
+}
+sw.Stop();
+
+Console.WriteLine($"time = {sw.ElapsedMilliseconds}");
+Console.WriteLine(max);
